934966318991a264cb8034cfe6d80210b676195f
el3alwawi <el3alwawi@hotmail.com>
2021-07-01 21:39:18 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
-    TOKEN: 'Your bot token here', 
-    YT_API_KEY: 'Youtube API Key here', 
-    prefix: 'prefix here',
+    TOKEN: 'ODYwMjU3NjcyMzkzNTIzMjMw.YN4nYw.GwhDGe3ct3ocEKgx0uk2wmBEoTE', 
+    YT_API_KEY: 'AIzaSyAL8akEvxIXXIhBKechZ7FktMKmSH8GyTo', 
+    prefix: '+',
     devs: ['your id/developers here']
 }
