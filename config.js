950bbe7e35662bd7935dc0c0c0e444c47bbd18f3
el3alwawi <el3alwawi@hotmail.com>
2021-07-10 19:52:09 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    TOKEN: 'ODUwMDY3NjQyMjgyMDE2NzY4.YLkVLw.LiHHqCUmEDnxHbIwHYGscC2H--A', 
+    TOKEN: 'ODUwMDY3NjQyMjgyMDE2NzY4.YLkVLw.gZQ82dj1bVtQji3LBi7m23mwzSE', 
     YT_API_KEY: 'AIzaSyCVVxTWy8cJUWCWQlahGCHqeACgrOMp3m0', 
     prefix: '-',
     devs: ['556833562549026816']
